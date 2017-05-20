0eae3e8d1800fd8bffd3c0c9af964c98796ceeb0
Robrecht Rikken <robin.rikken@gmail.com>
2017-05-20 23:48:17 +0200

    Card klasse afgemaakt
    Card klasse afgemaakt
    HelloTVXlet : Kaarten tonen op scherm

--- src/hellotvxlet/Card.java
+++ src/hellotvxlet/Card.java
@@ -1,6 +1,26 @@
 package hellotvxlet;
 
 import java.awt.Color;
+import java.awt.MediaTracker;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+import java.util.Collections;
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+import javax.tv.xlet.XletStateChangeException;
+import org.havi.ui.HComponent;
+import org.havi.ui.HScene;
+import org.havi.ui.HSceneFactory;
+import org.havi.ui.HVisible;
+import org.havi.ui.event.HActionListener;
+import java.awt.Image;
+import org.havi.ui.HBackgroundDevice;
+import org.havi.ui.HBackgroundImage;
+import org.havi.ui.HScreen;
+
+package hellotvxlet;
+
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import org.havi.ui.HGraphicButton;
@@ -13,5 +33,52 @@ import org.havi.ui.HVisible;
  */
 public class Card extends HGraphicButton
 {
+    Image front, back;
+    boolean blocked, turned = false;
+    int pair, width,height;
+    
+    public Card(int x, int y, String imgstring, int pair) {
+        this.pair = pair;
+        this.width = 54;
+        this.height = 75;
+        this.setLocation(x, y);
+        this.setSize(width, height);
+        this.setBackground(Color.WHITE);
+        this.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        this.setForeground(Color.BLACK);
+        front = this.getToolkit().getImage(imgstring);
+        back = this.getToolkit().getImage("images/Cards_0000_CardBack.jpg");
+        MediaTracker mt = new MediaTracker(this);
+        mt.addImage(front, 0);
+        mt.addImage(back, 1);
+        try {
+            mt.waitForAll();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.setGraphicContent(back, HState.NORMAL_STATE);
+        this.setGraphicContent(front, HState.ACTIONED_FOCUSED_STATE);
+    }
+    
+    //We choose the card to it faces the players
+    public void click() 
+    {
+        this.setGraphicContent(front, HState.NORMAL_STATE);
+        turned = true;
+    }
 
+    //The card did not form a pair so it needs to be flipped back down
+    public void reset()
+    {
+        if (!blocked) {
+            this.setGraphicContent(back, HState.NORMAL_STATE);
+            turned = false;
+        }
+    }
+
+    //Card is no longer available because it got paired.
+    public void block()
+    {
+        this.blocked = true;
+    }
 }
--- src/hellotvxlet/HelloTVXlet.java
+++ src/hellotvxlet/HelloTVXlet.java
@@ -27,13 +27,53 @@ public class HelloTVXlet extends HComponent implements Xlet, HActionListener
     HScene scene;
     Player[] players = new Player[2];
     int activePlayer = 0;
-   Image background; 
+    Image background; 
+    MediaTracker mt;
+    int countTurned = -1;
+     
    
+        CardCombo[] combos = {
+        new CardCombo(1, "images/Cards_0001_Kakuna.jpg"), new CardCombo(2, "images/Cards_0002_Charmander.jpg"),
+        new CardCombo(3, "images/Cards_0003_Caterpie.jpg"), new CardCombo(4, "images/Cards_0004_Bulbasaur.jpg"),
+        new CardCombo(5, "images/Cards_0005_Horsea.jpg"), new CardCombo(6, "images/Cards_0006_Umbreon.jpg"),
+        new CardCombo(7, "images/Cards_0007_Hoothoot.jpg"), new CardCombo(8, "images/Cards_0008_Clefable.jpg"),
+        new CardCombo(9, "images/Cards_0009_Krabby.jpg"), new CardCombo(10, "images/Cards_0010_Igglypuff.jpg"),
+        new CardCombo(11, "images/Cards_0011_Omanyte.jpg"), new CardCombo(12, "images/Cards_0012_Beedrill.jpg"),
+        new CardCombo(13, "images/Cards_0013_Beautifly.jpg"), new CardCombo(14, "images/Cards_0014_Marill.jpg"),
+        new CardCombo(15, "images/Cards_0015_Onyx.jpg"), new CardCombo(16, "images/Cards_0016_Parasect.jpg"),
+        new CardCombo(17, "images/Cards_0017_Pichu.jpg"), new CardCombo(18, "images/Cards_0018_Pikachu.jpg"),
+        new CardCombo(19, "images/Cards_0019_Sandslash.jpg"), new CardCombo(20, "images/Cards_0020_Sandshrew.jpg"),
+        new CardCombo(21, "images/Cards_0021_Seadra.jpg"), new CardCombo(22, "images/Cards_0022_Wooper.jpg"),
+        new CardCombo(23, "images/Cards_0023_Tentacool.jpg"), new CardCombo(24, "images/Cards_0024_Tentacruel.jpg"),
+         new CardCombo(1, "images/Cards_0001_Kakuna.jpg"), new CardCombo(2, "images/Cards_0002_Charmander.jpg"),
+        new CardCombo(3, "images/Cards_0003_Caterpie.jpg"), new CardCombo(4, "images/Cards_0004_Bulbasaur.jpg"),
+        new CardCombo(5, "images/Cards_0005_Horsea.jpg"), new CardCombo(6, "images/Cards_0006_Umbreon.jpg"),
+        new CardCombo(7, "images/Cards_0007_Hoothoot.jpg"), new CardCombo(8, "images/Cards_0008_Clefable.jpg"),
+        new CardCombo(9, "images/Cards_0009_Krabby.jpg"), new CardCombo(10, "images/Cards_0010_Igglypuff.jpg"),
+        new CardCombo(11, "images/Cards_0011_Omanyte.jpg"), new CardCombo(12, "images/Cards_0012_Beedrill.jpg"),
+        new CardCombo(13, "images/Cards_0013_Beautifly.jpg"), new CardCombo(14, "images/Cards_0014_Marill.jpg"),
+        new CardCombo(15, "images/Cards_0015_Onyx.jpg"), new CardCombo(16, "images/Cards_0016_Parasect.jpg"),
+        new CardCombo(17, "images/Cards_0017_Pichu.jpg"), new CardCombo(18, "images/Cards_0018_Pikachu.jpg"),
+        new CardCombo(19, "images/Cards_0019_Sandslash.jpg"), new CardCombo(20, "images/Cards_0020_Sandshrew.jpg"),
+        new CardCombo(21, "images/Cards_0021_Seadra.jpg"), new CardCombo(22, "images/Cards_0022_Wooper.jpg"),
+        new CardCombo(23, "images/Cards_0023_Tentacool.jpg"), new CardCombo(24, "images/Cards_0024_Tentacruel.jpg")
+    };
    
-   
+         Card[] cards = new Card[combos.length];
+         Card[] turnedCards = new Card[2];
+ 
    
  
-    class CardCombo {
+    class CardCombo 
+    {
+        int pair;
+        String imgString;
+        
+        public CardCombo(int pair, String imgString)
+        {
+            this.pair = pair;
+            this.imgString = imgString;
+        }
       
     }
 
@@ -44,16 +84,78 @@ public class HelloTVXlet extends HComponent implements Xlet, HActionListener
         scene.setBackgroundImage(background);
         scene.setRenderMode(HScene.IMAGE_CENTER);
        
+        //Mix cards
+        Collections.shuffle(Arrays.asList(combos));
         
-        // spelers aanmaken
+        //Build the array for the cards
+        int x = 70, y = 10;
+        for (int i = 0; i < combos.length; i++)
+        {
+            x += 74; //margin between cards
+            if (i % 8 == 0)//start next row
+            {
+                x = 70;
+                y += 80;
+            }
+            cards[i] = new Card(x, y, combos[i].imgString, combos[i].pair);
+            cards[i].setActionCommand(Integer.toString(i));
+            cards[i].addHActionListener(this);
+        }
+        
+        // sort navigation
+        int[] nextArray = {7, 15, 23, 31, 39, 47}; //indicates end of rows
+        for (int i = 0; i < cards.length; i++)
+        {
+            int prev = i - 1;
+            if (i % 8 == 0) {
+                prev = i + 7;
+            } else
+            {
+                if (prev < 0)
+                {
+                    prev = cards.length - 1;
+                }
+            }
+            boolean foundIndex = false;
+            int next = i + 1;
+            for (int j = 0; j < nextArray.length; j++)
+            {
+                if (nextArray[j] == i)
+                {
+                    foundIndex = true;
+                    next = i - 7;
+                }
+            }
+            if (!foundIndex) {
+                if (next > cards.length - 1) 
+                {
+                    next = 0;
+                }
+            }
+            int down = i + 8;
+            if (down >= cards.length)
+            {
+                down -= 48;
+            }
+            int up = i - 8;
+            if (up < 0) 
+            {
+                up += 48;
+            }
+            cards[i].setFocusTraversal(cards[up], cards[down], cards[prev], cards[next]);
+            scene.add(cards[i]);
+        }
+        // define players
           players[0] = new Player("Player 1","images/Player1.png", 72, 5, 125, 80);
         players[1] = new Player("Player 2","images/Player2.png", 500, 5, 125, 80);
         
-          //spelers aan scene toevoegen
+          //add players to scene
         scene.add(players[0]);
         scene.add(players[1]);
         scene.validate();
         scene.setVisible(true);
+        //Focus first card, so e can start navigating
+        cards[0].requestFocus();
         players[activePlayer].Activate();
         
     }
